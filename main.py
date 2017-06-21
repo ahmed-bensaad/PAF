f84762f6a1c5fef70ddf1b44cf867098a69fc400
Ahmed Ben Saad <abensaad@c127-01.enst.fr>
2017-06-21 15:42:28 +0200

    modification mineure

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import requests
 from requests.auth import HTTPBasicAuth
-import dic
 import Download as dn
-import ast
 import json
+
 # our demo filter that filters by geometry, date and cloud cover
 from demo_filters import redding_reservoir
 os.environ["PLANET_API_KEY"]="ae618a9b4c4448d4a1fbd71851ce835b"
@@ -20,11 +19,9 @@ result = \
     auth=HTTPBasicAuth(os.environ['PLANET_API_KEY'], ''),
     json=search_endpoint_request)
     
-print result.text    
-#a=dic.dictionnaire(result.text)
-#ast.literal_eval(result.text)
-#print a
+   
 a=json.loads(result.text)
+#print a
 L= [a['features'][i]['id'] for i in range(len(a['features']))]
 #print L
 
